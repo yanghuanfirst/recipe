ff95bbb591575d7e17fe7fbde267754d8c35097b
yanghuan <yang@xway.cn>
2025-03-03 14:27:22 +0800

    1

--- src/migrations/m230225_123456_create_recipe_table.php
+++ src/migrations/m230225_123456_create_recipe_table.php
@@ -13,7 +13,7 @@ class m230225_123456_create_recipe_table extends Migration
         $this->createTable('{{%recipe}}', [
             'id' => $this->primaryKey()->unsigned()->comment('主键ID'),
             'title' => $this->string(200)->notNull()->defaultValue('')->comment('食谱标题'),
-            'cover_img' => $this->string(100)->notNull()->defaultValue('')->comment('封面图片'),
+            'cover_img' => $this->string(255)->notNull()->defaultValue('')->comment('封面图片'),
             'type' => $this->smallInteger(3)->defaultValue(null)->comment('类型'),
             'recommend' => $this->tinyInteger(1)->notNull()->defaultValue(1)->comment('1：不推荐 2：推荐'),
             'detail' => $this->text()->notNull()->comment('详细内容'),
